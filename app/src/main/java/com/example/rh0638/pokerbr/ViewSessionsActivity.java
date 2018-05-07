8434fcfefcb6aca5f6cf3e1d9681a48c72de00b0
Holban <rh0638@live.missouristate.edu>
2018-05-07 13:19:22 -0500

    Created appbar and add/view session activity

--- app/src/main/java/com/example/rh0638/pokerbr/ViewSessionsActivity.java
+++ app/src/main/java/com/example/rh0638/pokerbr/ViewSessionsActivity.java
@@ -1,44 +1,28 @@
 package com.example.rh0638.pokerbr;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewSessionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_sessions);
 
         // Set the toolbar as the action bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-//        PokerDatabaseHelper pokerDatabaseHelper = new PokerDatabaseHelper(this);
-//
-//        SQLiteDatabase db = pokerDatabaseHelper.getWritableDatabase();
-//        pokerDatabaseHelper.insertSession(db, 2, "16:00", "20:00", 300, 421);
-//        pokerDatabaseHelper.insertSession(db, 2, "22:00", "01:00", 300, 421);
-//        db.close();
-//
-//        SQLiteDatabase db2 = pokerDatabaseHelper.getReadableDatabase();
-//        Cursor cursor = db2.query("Session", new String[] {"start_time", "end_time"}, null, null, null, null, null);
-//        cursor.moveToLast();
-//
-//
-//        LocalTime startTime = LocalTime.parse(cursor.getString(0));
-//        LocalTime endTime = LocalTime.parse(cursor.getString(1));
-//
-//        System.out.println("");
-//        System.out.println("PRINTING DEBUGGING INFO");
-//        System.out.println(endTime.getHour() - startTime.getHour());
-//        System.out.println("");
+        // Setup "UP" button
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     // Add any items in the menu_main to the app bar
@@ -62,4 +46,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
